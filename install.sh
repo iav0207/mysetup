018f19a8d6f02108d5f20640932c65f04c1066f5
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-11 17:27:04 +0100

    brew install nvim @ install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/bash
 
+set -e
+
 repo_root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rhome=$repo_root/replica/home
 
 main() {
     link_home_files
+    brew install nvim
 }
 
 link_home_files() {
     for f in $(find $rhome/dot -type f -maxdepth 1); do
-        link_path=~/.$(basename $f)
-	link $link_path $f
+        link ~/.$(basename $f) $f
     done
     link ~/.config/nvim $rhome/dot/config/nvim
 }
 
 link() {
     from=$1 to=$2
-    if [ -e $from ]; then log_and_do mv $from ${from}.bak; fi
+    if [ -e $from ]; then
+        log_and_do "rm -rf ${from}.bak && mv -f $from ${from}.bak"
+    fi
     ln -sf $to $from
 }
 
