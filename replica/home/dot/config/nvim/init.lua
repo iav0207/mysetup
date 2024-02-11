9f8b6b1174126db5d718697fb28ae80a365a0b89
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-11 18:52:15 +0100

    Modularize nvim configs

--- replica/home/dot/config/nvim/init.lua
+++ replica/home/dot/config/nvim/init.lua
@@ -1,7 +1,3 @@
-vim.cmd("source ~/.vimrc")
-
--- Lazy setup
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -15,29 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("base")
 require("lazy").setup("plugins")
-
--- Color scheme
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
--- Telescope
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
--- Treesitter
-
-require("nvim-treesitter.configs").setup({
-    ensure_installed = {"lua", "kotlin", "javascript", "haskell", "vim"},
-    highlight = { enable = true },
-    indent = { enable = true },
-})
-
--- NeoTree
-
-vim.keymap.set('n', '<C-n>', ':Neotree<CR>')
-
 
