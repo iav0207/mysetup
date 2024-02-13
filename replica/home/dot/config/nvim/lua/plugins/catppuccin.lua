fa59b37db2c4d9cc38d3fbc2ab349b484f69f915
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-13 22:54:39 +0100

    Add Catppuccin integrations with other plugins, no idea what these do

--- replica/home/dot/config/nvim/lua/plugins/catppuccin.lua
+++ replica/home/dot/config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,18 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin"
-    end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+
+		require("catppuccin").setup({
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+			},
+		})
+	end,
 }
