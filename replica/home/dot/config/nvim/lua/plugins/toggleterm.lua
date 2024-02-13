5351b0a5ed8d2d25012271deefef91acc48bcbd8
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-13 20:09:43 +0100

    Add ToggleTerm plugin

---
+++ replica/home/dot/config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			start_in_insert = true,
+			direction = "vertical",
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
+		})
+		vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", {})
+		vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", {})
+	end,
+}
