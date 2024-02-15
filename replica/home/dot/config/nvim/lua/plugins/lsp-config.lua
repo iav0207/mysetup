2bdb8717ac06d15a062ec5f9f9cd0ba4234ea0cc
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-15 20:22:39 +0100

    Additional key mapping to show diagnostic

--- replica/home/dot/config/nvim/lua/plugins/lsp-config.lua
+++ replica/home/dot/config/nvim/lua/plugins/lsp-config.lua
@@ -30,6 +30,7 @@ return {
             -- Global mappings.
             -- See `:help vim.diagnostic.*` for documentation on any of the below functions
             vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+            vim.keymap.set("n", "<leader>i", vim.diagnostic.open_float)
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
             vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
