be97f1265aecfd36847da0c62df0fa641fe4a95d
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-18 15:12:19 +0400

    Note @ haskell-tools.lua

--- replica/home/dot/config/nvim/lua/plugins/haskell-tools.lua
+++ replica/home/dot/config/nvim/lua/plugins/haskell-tools.lua
@@ -2,4 +2,7 @@ return {
     'mrcjkb/haskell-tools.nvim',
     version = '^3', -- Recommended
     ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
+    -- To fix renaming I nees to set
+    -- haskell.plugin.rename.config.crossModule": true
+    -- but idk where exactly.
 }
