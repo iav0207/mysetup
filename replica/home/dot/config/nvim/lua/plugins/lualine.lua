88ee9680e1f5ee1ebc95126ec45b8aeded71093d
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-23 10:52:40 +0700

    Switch to gruvbox theme

--- replica/home/dot/config/nvim/lua/plugins/lualine.lua
+++ replica/home/dot/config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
     requires = {'nvim-tree/nvim-web-devicons', opt = true },
     config = function()
         require('lualine').setup({
-            options = { theme = 'dracula' }
+            -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+            options = { theme = 'gruvbox_dark' }
         })
     end
 }
--- replica/home/dot/config/nvim/lua/plugins/themer.lua
+++ replica/home/dot/config/nvim/lua/plugins/themer.lua
@@ -2,8 +2,7 @@ return {
 	"themercorp/themer.lua",
 	config = function()
 		require("themer").setup({
-            colorscheme = "catppuccin",
-			-- colorscheme = "gruvbox",
+			colorscheme = "gruvbox-material-dark-hard",
             -- To cycle through the list of available color schemes:
             -- :lua require("telescope").load_extension("themes")
             -- :Telescope themes
