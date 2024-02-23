91504e2357fd59e39722596b9a31a6e8ed39e3f9
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-23 10:36:22 +0700

    Reformat neotree.lua

--- replica/home/dot/config/nvim/lua/plugins/neotree.lua
+++ replica/home/dot/config/nvim/lua/plugins/neotree.lua
@@ -1,28 +1,28 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
-        require('neo-tree').setup({
-            window = { width = 30 },
-            buffers = { follow_current_file = true },
-            filesystem = {
-                follow_current_file = true,
-                filtered_items = {
-                    hide_dotfiles = false,
-                    hide_gitignored = false,
-                    hide_by_name = {
-                        -- example: "node_modules",
-                    },
-                    never_show = { ".DS_Store" },
-                },
-            },
-        })
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		require("neo-tree").setup({
+			window = { width = 30 },
+			buffers = { follow_current_file = true },
+			filesystem = {
+				follow_current_file = true,
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						-- example: "node_modules",
+					},
+					never_show = { ".DS_Store" },
+				},
+			},
+		})
+	end,
 }
