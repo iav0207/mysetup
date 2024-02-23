f9ae8fa7ce6e6de43693caff3f559b89ff89835d
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-23 14:33:59 +0700

    Diffview history for the current file @ git.lua

--- replica/home/dot/config/nvim/lua/plugins/git.lua
+++ replica/home/dot/config/nvim/lua/plugins/git.lua
@@ -11,7 +11,7 @@ return {
         config = function()
             vim.keymap.set("n", "<leader>dvo", ":DiffviewOpen<CR>", {})
             vim.keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>", {})
-            vim.keymap.set("n", "<leader>dvh", ":DiffviewFileHistory<CR>", {})
+            vim.keymap.set("n", "<leader>dvh", ":DiffviewFileHistory %<CR>", {})
         end,
     },
 }
