c5cf6f8fef3e1e373b5ff3248ba078e26448b9dd
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-23 14:43:55 +0700

    Reformat @ lualine.lua

--- replica/home/dot/config/nvim/lua/plugins/lualine.lua
+++ replica/home/dot/config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,10 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    requires = {'nvim-tree/nvim-web-devicons', opt = true },
-    config = function()
-        require('lualine').setup({
-            -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-            options = { theme = 'auto' },
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	config = function()
+		require("lualine").setup({
+			-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+			options = { theme = "auto" },
+		})
+	end,
 }
