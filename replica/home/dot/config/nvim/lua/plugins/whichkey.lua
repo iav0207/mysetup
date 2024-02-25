902567bbe53abc263b8f425e2691f9e611bb1aee
Alexander Isaev <IsaevAV27@gmail.com>
2024-02-25 21:13:49 +0700

    Reformat @ whichkey.lua

--- replica/home/dot/config/nvim/lua/plugins/whichkey.lua
+++ replica/home/dot/config/nvim/lua/plugins/whichkey.lua
@@ -1,33 +1,33 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		-- Options: https://github.com/folke/which-key.nvim?tab=readme-ov-file#-setup
-		-- Custom mappings: https://github.com/folke/which-key.nvim?tab=readme-ov-file#-setup
-	},
-	config = function()
-		local wk = require("which-key")
-		wk.register({
-			["<C-l>"] = "Reset search",
-			["<C-p>"] = "Find files",
-		})
-		wk.register({
-			rn = "Rename identifier",
-			fg = "File grep",
-			dv = {
-				name = "Diffview",
-				o = "Open",
-				c = "Close",
-				h = "File history",
-			},
-			ac = "Toggle autocomplete",
-		}, {
-			prefix = "<leader>",
-			mode = "n",
-		})
-	end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- Options: https://github.com/folke/which-key.nvim?tab=readme-ov-file#-setup
+        -- Custom mappings: https://github.com/folke/which-key.nvim?tab=readme-ov-file#-setup
+    },
+    config = function()
+        local wk = require("which-key")
+        wk.register({
+            ["<C-l>"] = "Reset search",
+            ["<C-p>"] = "Find files",
+        })
+        wk.register({
+            rn = "Rename identifier",
+            fg = "File grep",
+            dv = {
+                name = "Diffview",
+                o = "Open",
+                c = "Close",
+                h = "File history",
+            },
+            ac = "Toggle autocomplete",
+        }, {
+            prefix = "<leader>",
+            mode = "n",
+        })
+    end,
 }
