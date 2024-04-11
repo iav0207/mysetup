995ed6c164a741480d9bf03540c90f153c5dc8db
Alexander Isaev <IsaevAV27@gmail.com>
2024-04-11 14:22:59 +0200

    New keymap for horizontal toggleterm

--- replica/home/dot/config/nvim/lua/plugins/toggleterm.lua
+++ replica/home/dot/config/nvim/lua/plugins/toggleterm.lua
@@ -16,6 +16,7 @@ return {
 		})
 		vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", {})
 		vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", {})
+		vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", {})
 		vim.keymap.set("n", "<leader>tg", ":TermExec direction=float cmd=lazygit<CR>", {})
 	end,
 }
