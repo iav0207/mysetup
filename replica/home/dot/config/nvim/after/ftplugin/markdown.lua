9a4480021e1e646b8911006a6a32e76f0fbeaa39
Alexander Isaev <IsaevAV27@gmail.com>
2024-04-26 16:35:01 +0200

    Fix @ ftplugin/markdown.lua

--- replica/home/dot/config/nvim/after/ftplugin/markdown.lua
+++ replica/home/dot/config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,7 @@
+local bufnr = vim.api.nvim_get_current_buf()
+local opts = { noremap = true, silent = true, buffer = bufnr, }
+
 -- en/em dashes
-vim.keymap.set('i', '--<space>', '–<space>')
-vim.keymap.set('i', '---<space>', '—<space>')
+vim.keymap.set('i', '--<space>', '–<space>', opts)
+vim.keymap.set('i', '---<space>', '—<space>', opts)
 
